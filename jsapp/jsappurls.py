4bb5e8cd7a0a3afbb93fe18e8892bee901963a50
Surja11 <stha.surja123@gmail.com>
2025-08-07 14:48:34 +0545

    Managed typos

--- jsapp/jsappurls.py
+++ jsapp/jsappurls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('', views.jshome, name='jshome'),  
+    path('home/', views.jshome, name='jshome'),  
     # path('jsapp/',views.jshome,name='jshome'),
     path('viewjobs/',views.viewjobs,name='viewjobs'),
     path('logout/',views.logout,name='logout'),
